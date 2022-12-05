c7673356e2e8a8fdbec68b4cca32dea6e51e0542
WiktorKuch <wiktor.kucharek1997@gmail.com>
2022-12-05 16:52:28 +0100

    AirbnbSkanowanieNakładającychSieTerminów

---
+++ AirbnbSkanowanieNakładającychSieTerminów/AirbnbSkanowanieNakładającychSieTerminów/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirbnbSkanowanieNakładającychSieTerminów
+{
+     class Reservation
+    {
+
+        public Reservation(DateTime @from ,DateTime to) 
+        {
+            From = from;
+            To = to;
+
+        }
+
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+
+
+}
